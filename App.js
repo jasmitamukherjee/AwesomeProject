2069b329a491a0e3f66ed29e2076281463ed3448
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-30 16:05:24 +0530

    added vector icons

--- App.js
+++ App.js
@@ -28,45 +28,22 @@ import {
 import Fontisto from 'react-native-vector-icons/Fontisto'
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+ 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
+      
      <View>
       <Text>
       ConnectIn
       </Text>
-      <Fontisto name='email' size={25} color='black'/>
+      <Fontisto name='email' size={26} color='black'/>
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+ 
 });
 
 export default App;
