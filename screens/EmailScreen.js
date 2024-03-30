53471e13cf3994001d47100ea8bfdd247f553c1e
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-31 00:11:18 +0530

    Update EmailScreen.js

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -1,11 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,SafeAreaView,Image, TextInput,TouchableOpacity } from 'react-native'
+import React, { useState} from 'react'
+import { useNavigation } from '@react-navigation/native'
 
+import Fontisto
+from 'react-native-vector-icons/Fontisto'
+import MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons'
 const EmailScreen = () => {
+  const [email,setEmail] = useState("")
+  const navigation=useNavigation()
+  const handleNext=()=>{
+    navigation.navigate("Password")
+
+  }
   return (
-    <View>
-      <Text>EmailScreen</Text>
+    <SafeAreaView style={{flex:1,backgroundColor:"white"}}>
+      <View style={{marginTop: 90, marginHorizontal: 20}}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{width:44,height:44,borderRadius:22,borderWidth:2,borderColor:"black",justifyContent:"center",alignItems:"center"}}>
+      <Fontisto name="email" size={26} color="black"/>
+
+      </View>
+      <Image  style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}/>
     </View>
+    <Text   style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+           color:"black",
+            marginTop: 15,
+          }}>Please provide a valid email</Text>
+           <Text style={{marginTop: 30, fontSize: 15, color: 'gray'}}>
+          Email verification helps us keep your account secure and let's you connect over the network more easily.
+          
+        </Text>
+          <TextInput autoFocus={true} value={email} onChange={(text)=>setEmail(text)} 
+          style={{
+            width: 340,
+            marginVertical: 10,
+            fontSize: email ? 22 : 22,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+           
+          }}placeholder='Enter your email' placeholderTextColor={'#BEBEBE'} />
+          <Text  style={{color: 'gray', fontSize: 15, marginTop: 7}}>Note : You'll be asked to verify your email.</Text>
+          <TouchableOpacity
+onPress={handleNext}
+activeOpacity={0.8} style={{marginTop:40,marginLeft:"auto"}}>
+<MaterialCommunityIcons 
+style={{alignSelf:"center",marginTop:20}}name="arrow-right-circle" size={45} color="#502b63"/>
+</TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
