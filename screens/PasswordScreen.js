e8eadb9c42db280f787049a087e61be5c7c48e73
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-31 00:11:25 +0530

    Update PasswordScreen.js

--- screens/PasswordScreen.js
+++ screens/PasswordScreen.js
@@ -1,11 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,SafeAreaView,Image,TextInput} from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
+import AntDesign
+from 'react-native-vector-icons/AntDesign'
+import { useNavigation } from '@react-navigation/native'
+import MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const PasswordScreen = () => {
+
+  const [password, setPassword] = useState("")
+  const navigation=useNavigation()
+  const handleNext=()=>{
+    navigation.navigate("Birth")
+
+  }
   return (
-    <View>
-      <Text>PasswordScreen</Text>
+    <SafeAreaView style={{flex:1}}>
+      <View style={{marginTop:90,marginHorizontal:20}}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{width:44,height:44,borderRadius:22,borderWidth:2,borderColor:"black",justifyContent:"center",alignItems:"center"}}>
+      <AntDesign name="lock1" size={26} color="black"/>
+
+      </View>
+      <Image  style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}/>
     </View>
+    <Text style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            color:"black",
+            marginTop: 15,
+          }}>Please choose a password</Text>
+           <TextInput autoFocus={true} secureTextEntry={true} value={password} onChange={(text)=>setPassword(text)} 
+          style={{
+            width: 340,
+            marginVertical: 10,
+            fontSize: password ? 22 : 22,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            
+           
+          }}placeholder='Enter your password' placeholderTextColor={'#BEBEBE'} />
+           <TouchableOpacity
+onPress={handleNext}
+activeOpacity={0.8} style={{marginTop:40,marginLeft:"auto"}}>
+<MaterialCommunityIcons 
+style={{alignSelf:"center",marginTop:20}}name="arrow-right-circle" size={45} color="#502b63"/>
+</TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
