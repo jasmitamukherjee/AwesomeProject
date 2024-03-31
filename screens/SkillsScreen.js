4f31514b03532b10a071c1475e44f900326f5496
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-31 16:00:13 +0530

    updated

--- screens/SkillsScreen.js
+++ screens/SkillsScreen.js
@@ -1,11 +1,139 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,Image, FlatList ,TextInput,Button} from 'react-native'
 import React from 'react'
+import MaterialIcons
+from 'react-native-vector-icons/MaterialIcons'
+import AntDesign
+from 'react-native-vector-icons/AntDesign'
+import { useState } from 'react'
+import DocumentPicker from 'react-native-document-picker'
+import MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useNavigation } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native'
 
 const SkillsScreen = () => {
+
+  const navigation=useNavigation()
+  const handleNext=()=>{
+    navigation.navigate("LookingForEmployee")
+
+  }
+  const [resumeUri, setResumeUri] = useState("");
+
+  const [skillInput, setSkillInput] = useState('');
+  const [skills, setSkills] = useState([]);
+
+  const handleAddSkill = () => {
+    if (skillInput.trim() !== '') {
+      setSkills([...skills, skillInput]);
+      setSkillInput('');
+    }
+  };
+  const handleUploadResume = async () => {
+    try {
+      const res = await DocumentPicker.pickSingle({
+        type: [DocumentPicker.types.pdf]
+      })
+      setResumeUri(res.name);
+      // console.log(res)
+      // console.log(resumeUri)
+     
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        console.error("User cancelled upload",err);
+      }
+      else{
+        console.log(err)
+      }
+    }
+  };
   return (
-    <View>
-      <Text>SkillsScreen</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{marginTop: 90, marginHorizontal: 20}}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{width:44,height:44,borderRadius:22,borderWidth:2,borderColor:"black",justifyContent:"center",alignItems:"center"}}>
+      <MaterialIcons name="work-outline" size={26} color="black"/>
+
+      </View>
+      <Image  style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}/>
     </View>
+    <Text   style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+           color:"black",
+            marginTop: 15,
+          }}>Enter your Skills</Text>
+
+
+          {/* new added  */}
+
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+          <TextInput
+            style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1, paddingHorizontal: 10,borderRadius: 10 }}
+            placeholder="Enter skill"
+            value={skillInput}
+            onChangeText={text => setSkillInput(text)}
+          />
+          <View style={{width:40}}>
+          <MaterialIcons.Button name="add" backgroundColor="#502b63" borderRadius={30} width={50} onPress={handleAddSkill}></MaterialIcons.Button>
+          </View>
+        </View>
+        
+        <Text style={{ fontSize: 15, fontWeight:700,color: 'gray', marginTop: 15 }}>Entered Skills</Text>
+        
+        <FlatList
+          data={skills}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+<View style={{flexDirection:"row"}}>
+<Text   style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+           color:"black",
+            marginTop: 15,
+          }}>Upload your Resume</Text>
+
+<View style={{width:40,marginLeft:50,marginTop:15,}}>
+<AntDesign.Button name="upload" borderRadius={30} 
+  width={50}  backgroundColor="#502b63" onPress={handleUploadResume} />
+          
+        </View>
+
+        
+</View>
+{resumeUri && <Text style={{ color: "gray",fontWeight:600, marginTop: 10 }}>Resume Uploaded: {resumeUri}</Text>}
+
+
+{/* new add  */}
+
+{/* {resumeUri && (
+          <View style={{ flex: 1, marginTop: 20 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#502b63', marginBottom: 10 }}>Preview of Uploaded Resume</Text>
+            <WebView
+              source={{ uri: resumeUri }}
+              style={{ flex: 1 }}
+            />
+          </View>
+        )} */}
+
+
+<TouchableOpacity
+        onPress={handleNext}
+          activeOpacity={0.8}
+          style={{marginTop: 30, marginLeft: 'auto'}}>
+          <MaterialCommunityIcons
+            name="arrow-right-circle"
+            size={45}
+            color="#502b63"
+            style={{alignSelf: 'center', marginTop: 20}}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
--- screens/UserType.js
+++ screens/UserType.js
@@ -1,11 +1,109 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View , SafeAreaView,Image} from 'react-native'
+import React, { useState } from 'react'
+import FontAwesome5
+from 'react-native-vector-icons/FontAwesome5'
+import FontAwesome
+from 'react-native-vector-icons/FontAwesome'
+import AntDesign
+from 'react-native-vector-icons/AntDesign'
+import { Pressable } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useNavigation } from '@react-navigation/native'
 const UserType = () => {
+  const navigation=useNavigation()
+  const handleNext=()=>{
+    if(userType == 'Employee'){
+    navigation.navigate("Skills")}
+    else{
+      navigation.navigate("LookingFor")
+
+    }
+
+  }
+  const [userType,setUserType]= useState("")
   return (
-    <View>
-      <Text>UserType</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{marginTop: 90, marginHorizontal: 20}}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{width:44,height:44,borderRadius:22,borderWidth:2,borderColor:"black",justifyContent:"center",alignItems:"center"}}>
+      <FontAwesome5 name="user-tie" size={26} color="black"/>
+
+      </View>
+      <Image  style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}/>
     </View>
+    <Text   style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+           color:"black",
+            marginTop: 15,
+          }}>You're here for</Text>
+            <Text style={{marginTop: 30, fontSize: 15, color: 'gray'}}>
+          ConnectIn users are matched based on this selection.
+        </Text>
+
+        <View style={{marginTop: 30, flexDirection: 'column', gap: 12}}>
+          <View  style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}> 
+            <Text style={{fontWeight: '500', fontSize: 15,color:"black"}}>I am here to look for jobs.</Text>
+            <Pressable onPress={() => setUserType('Employee')}>
+              <FontAwesome
+                name="circle"
+                size={26}
+                color={userType == 'Employee' ? '#581845' : '#F0F0F0'}
+              />
+            </Pressable>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{fontWeight: '500', fontSize: 15,color:"black"}}>I am here to hire.</Text>
+            <Pressable onPress={() => setUserType('Employer')}>
+              <FontAwesome
+                name="circle"
+                size={26}
+                color={userType == 'Employer' ? '#581845' : '#F0F0F0'}
+              />
+            </Pressable>
+          </View>
+          <View>
+            <Text>Note : You'll be shown as "{userType}" on this app.</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: 30,
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 8,
+          }}>
+          <AntDesign name="checksquare" size={26} color="#502b63" />
+          <Text style={{fontSize: 15}}>Visible on profile</Text>
+        </View>
+        <TouchableOpacity
+        onPress={handleNext}
+          activeOpacity={0.8}
+          style={{marginTop: 30, marginLeft: 'auto'}}>
+          <MaterialCommunityIcons
+            name="arrow-right-circle"
+            size={45}
+            color="#502b63"
+            style={{alignSelf: 'center', marginTop: 20}}
+          />
+        </TouchableOpacity>
+    
+    </View>
+    </SafeAreaView>
   )
 }
 
